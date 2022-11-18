29f7e4a4347e449f742ea3b42e1fe8af39d70c55
m-cakir <mcakir.web@gmail.com>
2022-11-18 15:53:09 +0300

    start.spring.io

---
+++ src/main/java/com/mcakir/demo/FeignOauth2InterceptorDemoApplication.java
@@ -0,0 +1,13 @@
+package com.mcakir.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeignOauth2InterceptorDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeignOauth2InterceptorDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mcakir/demo/FeignOauth2InterceptorDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mcakir.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeignOauth2InterceptorDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
